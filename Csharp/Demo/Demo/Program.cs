85785f887f2af0ebe3e5d0458a977b4e6dc0ba55
stephenliu <stephenlyao@yeah.net>
2016-10-01 22:23:50 +0800

    add some stuffs

---
+++ Csharp/Demo/Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Demo
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
---
+++ canvas-learning/rubberband/index.js
@@ -0,0 +1,12 @@
+var canvas = document.getElementById('canvas')
+var context = canvas.getContext('2d')
+var rubberbandDiv = document.getElementById('rubberbandDiv')
+var resetButton = document.getElementById('resetButton')
+var img = new Image()
+var mousedown = {}
+var rubberbandRectangle = {}
+var dragging = false
+
+function rubberbandStart(x,y){
+  
+}
