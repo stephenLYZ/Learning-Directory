6865a859f94a6979a4a2c27ef6a6cbade27a4fb1
stephenliu <stephenlyao@yeah.net>
2017-04-24 21:59:21 +0800

    rust1

---
+++ rust-learning/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/Users/stephenliu/Desktop/Learning-Directory/rust-learning/guessing_game/target/debug/guessing_game: /Users/stephenliu/Desktop/Learning-Directory/rust-learning/guessing_game/src/main.rs
---
+++ rust-learning/hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/stephenliu/Desktop/Learning-Directory/rust-learning/hello_world/target/debug/hello_world: /Users/stephenliu/Desktop/Learning-Directory/rust-learning/hello_world/src/main.rs
