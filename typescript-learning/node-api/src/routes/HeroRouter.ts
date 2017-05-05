41dd56e719ae552436650796e358a21d8d1d51aa
stephenliu <stephenlyao@yeah.net>
2017-05-05 23:56:15 +0800

    node-api

--- typescript-learning/node-api/src/routes/HeroRouter.ts
+++ typescript-learning/node-api/src/routes/HeroRouter.ts
@@ -13,9 +13,30 @@ export class HeroRouter {
     public getAll(req: Request, res: Response, next: NextFunction) {
         res.send(Heroes)
     }
+    
+    public getOne(req: Request, res: Response, next: NextFunction) {
+        let query = parseInt(req.params.id)
+        let hero = Heroes.find(hero => hero.id === query)
+
+        if(hero) {
+            res.status(200)
+                .send({
+                    message: 'Success',
+                    status: res.status,
+                    hero
+                })
+        } else {
+            res.status(404)
+                .send({
+                    message: 'No hero found with the given id.',
+                    status: res.status
+                })
+        }
+    }
 
     init() {
         this.router.get('/', this.getAll)
+        this.router.get('/:id', this.getOne)
     }
 }
 
